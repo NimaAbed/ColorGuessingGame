ba42c70c5438dd080df1f2b799a3c578d6b95622
Nima Abed <abodnima84@gmail.com>
2023-06-22 13:20:44 +0330

    Create Func SelectRandomColor

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const getMessage=document.querySelector("#message")
 let randomCont
 const getRGB = document.querySelector("#color-display")
 const container = document.querySelectorAll(".square")
+function selectRandomColor() {
+    //  for(let i=0 ; i<randomColor.length;i++){
+    //     randomColor[i]=Math.round(Math.random()*255)
+
+    //  }
+    randomColor = [randomNumber(), randomNumber(), randomNumber()]
+    getRGB.innerHTML = `RGB(${randomColor.join(",")})`
+    reset.addEventListener("click",ryLoad)
+    console.log(randomColor)
+    randomCont = container[Math.floor(Math.random() * container.length)]
+    randomCont.style.backgroundColor = `rgb(${randomColor.join(",")})`
+    randomCont.style.backgroundColor = `rgb(${randomColor.join(",")})`
+    container.forEach(item => {
+        if (item.style.backgroundColor != randomCont.style.backgroundColor) {
+            item.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`
+            // console.log(item)
+        }
+
+        console.log(item)
+        item.addEventListener("click", btnHandler)
+        // console.log(item.id)
+    })
+}
+
+
+
+selectRandomColor()
