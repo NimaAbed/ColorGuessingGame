9733fe2d1cb77408637494bf286df316c5324e7e
Nima Abed <abodnima84@gmail.com>
2023-06-22 13:19:49 +0330

    Create Some Const

---
+++ script.js
@@ -0,0 +1,9 @@
+let randomColor = [0, 0, 0]
+let RGB = []
+let endGame=false
+const getH1=document.querySelector("h1")
+const reset=document.querySelector("#reset")
+const getMessage=document.querySelector("#message")
+let randomCont
+const getRGB = document.querySelector("#color-display")
+const container = document.querySelectorAll(".square")
